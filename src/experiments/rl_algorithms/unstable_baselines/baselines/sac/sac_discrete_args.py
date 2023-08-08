2ac281d485f634fee6f2ee2483253f374091411a
mariacristinapf <126967165+mariacristinapf@users.noreply.github.com>
2023-08-08 11:56:11 +0200

    Update SAC args to test in my other computer.

--- src/experiments/rl_algorithms/unstable_baselines/baselines/sac/sac_discrete_args.py
+++ src/experiments/rl_algorithms/unstable_baselines/baselines/sac/sac_discrete_args.py
@@ -10,32 +10,32 @@ def sac_args(self):
             "gamma": 0.99,
             "update_target_network_interval": 1,
             "target_smoothing_tau": 0.005,
-            "alpha": 0.2,
+            "alpha": 0.5,
             "reward_scale": 5.0,
             "q_network": {
                 "network_params": [("mlp", 64), ("mlp", 64), ("mlp", 64), ("mlp", 64)],
                 "optimizer_class": "Adam",
-                "learning_rate": 0.0003,
+                "learning_rate": 0.0005,
                 "act_fn": "relu",
                 "out_act_fn": "identity"
             },
             "policy_network": {
                 "network_params": [("mlp", 64), ("mlp", 64), ("mlp", 64), ("mlp", 64)],
                 "optimizer_class": "Adam",
-                "learning_rate": 0.0003,
+                "learning_rate": 0.0005,
                 "act_fn": "relu",
                 "out_act_fn": "identity",
             },
             "entropy": {
                 "automatic_tuning": True,
-                "learning_rate": 0.0003,
+                "learning_rate": 0.0005,
                 "optimizer_class": "Adam",
                 "scale": 0.5
             }
         },
         "trainer": {
             "max_env_steps": self.N_TIMESTEPS,
-            "batch_size": 256,
+            "batch_size": 128,
             "eval_interval": self.EVAL_FREQ,
             "num_eval_trajectories": self.N_EVAL_EPISODES,
             "snapshot_interval": 10000,
