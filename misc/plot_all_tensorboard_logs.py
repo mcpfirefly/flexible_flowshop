9854db2c7f87c5e36d2a1f37f97eca4d14926fdb
mariacristinapf <126967165+mariacristinapf@users.noreply.github.com>
2023-08-16 23:48:30 +0200

    Add function to generate all matplotlib graphs from tensorboard log files.

--- misc/plot_all_tensorboard_logs.py
+++ misc/plot_all_tensorboard_logs.py
@@ -2,7 +2,7 @@
 import matplotlib.pyplot as plt
 import pickle, os
 import tensorflow as tf
-
+import numpy as np
 # Function to generate and save plots
 def generate_and_save_plots(event_file, log_path):
 
@@ -15,14 +15,6 @@ def generate_and_save_plots(event_file, log_path):
             if value.HasField('simple_value'):
                 store_variables.setdefault(value.tag, []).append((event.wall_time, value.simple_value))
 
-    # Save data dictionary as pickle
-
-    pickle_filename = 'stored_variables.pkl'
-    save_pickle = os.path.join(log_path, pickle_filename)
-
-    with open(save_pickle, 'wb') as save_pickle:
-        pickle.dump(store_variables, save_pickle)
-
 
     # Smoothing factor
     smoothing_factor = 0.8
@@ -30,40 +22,94 @@ def generate_and_save_plots(event_file, log_path):
     # Create the base output directory if it doesn't exist
     os.makedirs(base_output_directory, exist_ok=True)
     # Create separate plots for each key in the dictionary
+    steps = event.step
+
     for key, value in store_variables.items():
         filename_safe_key = key.replace('/', '-')
+        # Generate the modified label for plotting
+        modified_label = key.split('/')[-1].title()
+        # Remove underscores and replace them with spaces
+        modified_label = modified_label.replace('_', ' ')
+
+        num_steps = len(value)
+        step_size = steps / (num_steps - 1)
+        # Generate x-values with steps of 1000
+        x_values = [i * step_size for i in range(num_steps)]
+
+        # Replace x-values in each tuple
+        updated_value = [(x, y) for x, (_, y) in zip(x_values, value)]
+
+        # Update the value in data_dict
+        store_variables[key] = updated_value
 
         # Extract x and y values from the list of tuples
-        x_values, y_values = zip(*value)
+        _, y_values = zip(*value)
 
-        # Apply exponential moving average (EMA) smoothing to y-values
-        smoothed_y_values = [y_values[0]]
-        for y in y_values[1:]:
-            smoothed_y = smoothing_factor * smoothed_y_values[-1] + (1 - smoothing_factor) * y
-            smoothed_y_values.append(smoothed_y)
 
-        # Create a new figure
-        plt.figure()
+        if smoothing:
+            # Apply exponential moving average (EMA) smoothing to y-values
+            smoothed_y_values = [y_values[0]]
+            for y in y_values[1:]:
+                smoothed_y = smoothing_factor * smoothed_y_values[-1] + (1 - smoothing_factor) * y
+                smoothed_y_values.append(smoothed_y)
 
-        # Plot the original data points in a lighter color
-        plt.scatter(x_values, y_values, color='gray', alpha=0.4, label=f'{filename_safe_key} (original)')
+            # Create a new figure
+            plt.figure()
 
-        # Plot the smoothed data
-        plt.plot(x_values, smoothed_y_values, label=f'{filename_safe_key} (smoothed)')
+            # Plot the original data points in a lighter color
+            plt.scatter(x_values, y_values, color='gray', alpha=0.4, label=f'{modified_label}')
 
-        # Set title and labels
-        plt.title(f'{filename_safe_key} with Smoothing')
-        plt.xlabel('X values')
-        plt.ylabel('Y values')
+            # Plot the smoothed data
+            plt.plot(x_values, smoothed_y_values, label=f'{modified_label} (smoothed: {smoothing_factor})')
 
-        # Add legend
-        plt.legend()
+            # Set title and labels
+            plt.title(f'{modified_label}')
+            plt.xlabel('Timesteps')
+            plt.ylabel(f'{modified_label}')
 
-        # Save the plot to the specified location
-        plot_filename = f'{filename_safe_key}_plot.png'  # You can use any desired file format (e.g., .eps, .jpg, etc.)
-        save_path = os.path.join(base_output_directory, plot_filename)
-        plt.savefig(save_path, dpi=400)
-        plt.close()
+            # Add legend
+            plt.legend()
+
+            # Save the plot to the specified location
+            plot_filename = f'{filename_safe_key}_plot.png'  # You can use any desired file format (e.g., .eps, .jpg, etc.)
+            save_path = os.path.join(base_output_directory, plot_filename)
+            plt.savefig(save_path, dpi=400)
+            plt.close()
+
+        elif moving_average:
+            # Calculate the moving average
+            moving_averages = np.convolve(y_values, np.ones(moving_average_period) / moving_average_period,
+                                          mode='valid')
+
+            # Create a new figure
+            plt.figure()
+
+            # Plot the original data points in a lighter color
+            plt.scatter(x_values, y_values, color='gray', alpha=0.4, label="Samples")
+
+            # Plot the moving average
+            plt.plot(x_values[moving_average_period - 1:], moving_averages, label="Average")
+
+            # Set title and labels
+            plt.title(f'{modified_label}')
+            plt.xlabel('Timesteps')
+            plt.ylabel(f'{modified_label}')
+
+            # Add legend
+            plt.legend()
+
+            # Save the plot to the specified location
+            plot_filename = f'{filename_safe_key}_plot.png'  # You can use any desired file format (e.g., .eps, .jpg, etc.)
+            save_path = os.path.join(base_output_directory, plot_filename)
+            plt.savefig(save_path, dpi=400)
+            plt.close()
+
+        # Save data dictionary as pickle
+        pickle_filename = 'stored_variables.pkl'
+        save_pickle = os.path.join(log_path, pickle_filename)
+
+        with open(save_pickle, 'wb') as save_pickle:
+            pickle.dump(store_variables, save_pickle)
 
 
 # Recursive function to process subdirectories and generate plots
@@ -77,6 +123,8 @@ def process_directory(directory_path):
 if __name__ == "__main__":
     # Define your base source directory
     base_source_directory = 'C:/Users/INOSIM/PycharmProjects/flexible_flowshop/src/experiments/results'
-
+    smoothing = False
+    moving_average = True
+    moving_average_period = 12
     # Start processing from the base source directory
     process_directory(base_source_directory)
